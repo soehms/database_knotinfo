bc520373b5e6652f578d2468aa7521f4a07e1fb9
Sebastian Oehms <seb.oehms@gmail.com>
2021-05-06 17:40:22 +0200

    increase version

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'database_knotinfo',
   packages = ['database_knotinfo'],
-  version = '0.3',
+  version = '0.4',
   license='GPL',
   description = 'Content of the KnotInfo and LinkInfo databases as lists of dictionaries',
   author = 'Sebastian Oehms',
   author_email = 'seb.oehms@gmail.com',
   url = 'https://github.com/soehms/database_knotinfo',
-  download_url = 'https://github.com/soehms/database_knotinfo/archive/pypi-0_3.tar.gz',
+  download_url = 'https://github.com/soehms/database_knotinfo/archive/pypi-0_4.tar.gz',
   keywords = ['KnotInfo', 'LinkInfo', 'SageMath', 'database'],
   install_requires=[
       ],
